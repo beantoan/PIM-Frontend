807f7e91a79b1dc075eea53f2003a3449eaaee90
Đậu Quốc Toản <beantoan@gmail.com>
2018-10-25 09:03:39 +0200

    Format input number

--- src/app/transaction-dialog/transaction-dialog.component.ts
+++ src/app/transaction-dialog/transaction-dialog.component.ts
@@ -281,15 +281,15 @@ export class TransactionDialogComponent implements OnInit, AfterViewInit {
     const data = this.transactionForm.value;
 
     if (data.quantity) {
-      data.quantity = data.quantity.replace(/\D+/g, '');
+      data.quantity = data.quantity.toString().replace(/\D+/g, '');
     }
 
     if (data.price) {
-      data.price = data.price.replace(/\D+/g, '');
+      data.price = data.price.toString().replace(/\D+/g, '');
     }
 
     if (data.money) {
-      data.money = data.money.replace(/\D+/g, '');
+      data.money = data.money.toString().replace(/\D+/g, '');
     }
 
     if (this.transaction) {
